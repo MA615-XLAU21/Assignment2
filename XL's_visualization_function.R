1e00489f72f022a30f494c3c8db2272270356d3e
XKL <xukl@bu.edu>
2021-10-31 13:16:01 -0400

    I didn't modify the essential content of the homework

--- XL's_visualization_function.R
+++ XL's_visualization_function.R
@@ -6,23 +6,29 @@
 #Then I choose top Five developed Countries
 #United States, United Kingdom and France,Poland,Spain
 visualization_function <- function(data_join, country1, country2, country3, country4, country5, indicators){
-
-if(indicators == "fertility"){
-  data1ed <- data_join %>%
+  newdata <- data_join %>%
     filter(country == country1)
-  data2ed <- data_join %>%
-    filter(country == country2)
-  data3ed <- data_join %>%
-    filter(country == country3)
-  data4ed <- data_join %>%
-    filter(country == country4)
-  data5ed <- data_join %>%
-    filter(country == country5)
-  newdata <- rbind(data.frame(country = data1ed$country, year = data1ed$year, fertility = data1ed$fertility),
-                   data.frame(country = data2ed$country, year = data2ed$year, fertility = data2ed$fertility),
-                   data.frame(country = data3ed$country, year = data3ed$year, fertility = data3ed$fertility),
-                   data.frame(country = data4ed$country, year = data4ed$year, fertility = data4ed$fertility),
-                   data.frame(country = data5ed$country, year = data5ed$year, fertility = data5ed$fertility))
+  newdata <- rbind(newdata,filter(data_join,country == country2))
+  newdata <- rbind(newdata,filter(data_join,country == country3))
+  newdata <- rbind(newdata,filter(data_join,country == country4))
+  newdata <- rbind(newdata,filter(data_join,country == country5))
+  
+if(indicators == "fertility"){
+  #data1ed <- data_join %>%
+  #  filter(country == country1)
+  #data2ed <- data_join %>%
+  #  filter(country == country2)
+  #data3ed <- data_join %>%
+  #  filter(country == country3)
+  #data4ed <- data_join %>%
+  #  filter(country == country4)
+  #data5ed <- data_join %>%
+  #  filter(country == country5)
+  ##newdata <- rbind(data.frame(country = data1ed$country, year = data1ed$year, fertility = data1ed$fertility),
+  ##                 data.frame(country = data2ed$country, year = data2ed$year, fertility = data2ed$fertility),
+  ##                 data.frame(country = data3ed$country, year = data3ed$year, fertility = data3ed$fertility),
+  ##                  data.frame(country = data4ed$country, year = data4ed$year, fertility = data4ed$fertility),
+  ##                 data.frame(country = data5ed$country, year = data5ed$year, fertility = data5ed$fertility))
  
   ggplot(data = newdata, mapping = aes(x = year, y = fertility))+
            geom_point(mapping = aes(color = country)) +
@@ -32,21 +38,21 @@ if(indicators == "fertility"){
 
 #if(indicators == "femployrate") 
   else{
-  data1ed <- data_join %>%
-    filter(country == country1)
-  data2ed <- data_join %>%
-    filter(country == country2)
-  data3ed <- data_join %>%
-    filter(country == country3)
-  data4ed <- data_join %>%
-    filter(country == country4)
-  data5ed <- data_join %>%
-    filter(country == country5)
-  newdata <- rbind(data.frame(country = data1ed$country, year = data1ed$year, femployrate = data1ed$femployrate),
-                   data.frame(country = data2ed$country, year = data2ed$year, femployrate = data2ed$femployrate),
-                   data.frame(country = data3ed$country, year = data3ed$year, femployrate = data3ed$femployrate),
-                   data.frame(country = data4ed$country, year = data4ed$year, femployrate = data4ed$femployrate),
-                   data.frame(country = data5ed$country, year = data5ed$year, femployrate = data5ed$femployrate))
+  #data1ed <- data_join %>%
+  #  filter(country == country1)
+  #data2ed <- data_join %>%
+  #  filter(country == country2)
+  #data3ed <- data_join %>%
+  #  filter(country == country3)
+  #data4ed <- data_join %>%
+  #  filter(country == country4)
+  #data5ed <- data_join %>%
+  #  filter(country == country5)
+  #newdata <- rbind(data.frame(country = data1ed$country, year = data1ed$year, femployrate = data1ed$femployrate),
+  #                 data.frame(country = data2ed$country, year = data2ed$year, femployrate = data2ed$femployrate),
+  #                 data.frame(country = data3ed$country, year = data3ed$year, femployrate = data3ed$femployrate),
+  #                 data.frame(country = data4ed$country, year = data4ed$year, femployrate = data4ed$femployrate),
+  #                 data.frame(country = data5ed$country, year = data5ed$year, femployrate = data5ed$femployrate))
 
   ggplot(data = newdata, mapping = aes(x = year, y = femployrate ))+
     geom_point(mapping = aes(color = country)) +
@@ -54,6 +60,10 @@ if(indicators == "fertility"){
   
 }
 }
-
+##I didn't modify the essential content of the homework, 
+##just modified the function part to be simpler. 
+##The main content remains the same, and the overall look will be more concise. 
+##Especially newdata does not need to be written twice, 
+##so it is easier to put it on the outermost layer
     
   
